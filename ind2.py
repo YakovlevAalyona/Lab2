04fd0eabbdcbb7e0c2ddaf9310f6067e90ba337c
YakovlevAalyona <93199050+YakovlevAalyona@users.noreply.github.com>
2021-12-16 21:46:17 +0300

    Add files via upload

---
+++ ind2.py
@@ -0,0 +1,12 @@
+a = float(input("Введите a:"))
+b = float(input("Введите b:"))
+c = float(input("Введите c:"))
+
+if (a<b, c<b):
+   print(a*c)
+   
+elif (b<a, c<a):
+    print(b*c)
+    
+else :
+    print(a*b)
